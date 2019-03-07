f8382bd4eac05caa732b94f820a2428cd1ed104c
Michal Bock <michal.bock@gmail.com>
2019-03-07 10:42:55 +0000

    Rename repo to sync.

--- gogroup/gogroup_test.go
+++ gogroup/gogroup_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"github.com/uw-labs/rungroup/gogroup"
+	"github.com/uw-labs/sync/gogroup"
 )
 
 func run(ctx context.Context, d time.Duration) error {
